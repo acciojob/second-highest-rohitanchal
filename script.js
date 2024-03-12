857bb6e0d43b1bbe1b332e456b20569570da40e1
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2024-03-13 00:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+function secondHighest(arr) {
+    if (arr.length < 2 || new Set(arr).size === 1) {
+        return -Infinity;
+    }
+
+    let firstMax = -Infinity;
+    let secondMax = -Infinity;
+
+    for (let num of arr) {
+        if (num > firstMax) {
+            secondMax = firstMax;
+            firstMax = num;
+        } else if (num > secondMax && num < firstMax) {
+            secondMax = num;
+        }
+    }
+
+    return secondMax;
+}
+
